851b654b004c48853b2b9b5dc5666519fe1ade12
Jordan McCullough <mccj01@TheIsland.local>
2011-01-04 16:50:23 -0700

    -First check in of blog style for WordPress

---
+++ blog/wp-content/themes/ai-custom/archive.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Default_Theme
+ */
+
+get_header(); ?>
+
+	<div id="content" class="narrowcolumn">
+
+	<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
+ 	  <?php /* If this is a category archive */ if (is_category()) { ?>
+		<h1>Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category</h1>
+ 	  <?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
+		<h1>Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;</h1>
+ 	  <?php /* If this is a daily archive */ } elseif (is_day()) { ?>
+		<h1>Archive for <?php the_time('F jS, Y'); ?></h1>
+ 	  <?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
+		<h1>Archive for <?php the_time('F, Y'); ?></h1>
+ 	  <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
+		<h1>Archive for <?php the_time('Y'); ?></h1>
+	  <?php /* If this is an author archive */ } elseif (is_author()) { ?>
+		<h1>Author Archive</h1>
+ 	  <?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
+		<h1>Blog Archives</h1>
+ 	  <?php } ?>
+
+	<?php if (have_posts()) : ?>
+
+		<?php while (have_posts()) : the_post(); ?>
+
+			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+				<small><?php the_time('F jS, Y') ?> <!-- by <?php the_author() ?> --></small>
+
+				<div class="entry">
+					<?php the_content('Read the rest of this entry &raquo;'); ?>
+				</div>
+
+				<p class="postmetadata"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
+			</div>
+
+		<?php endwhile; ?>
+
+		<div class="navigation">
+			<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
+			<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+		</div>
+
+	<?php else : ?>
+
+		<h2 class="center">Not Found</h2>
+		<p class="center">Sorry, but you are looking for something that isn't here.</p>
+		<?php get_search_form(); ?>
+
+	<?php endif; ?>
+
+	</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
